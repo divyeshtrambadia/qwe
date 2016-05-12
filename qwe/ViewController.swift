c310c5b79812addb0df652b45fef9e7efc6ea6d4
Kirit Modi <kiritmodi27021991@gmail.com>
2016-05-12 17:12:45 +0530

    awe
    awe

---
+++ qwe/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  qwe
+//
+//  Created by Nimble Chapps on 08/04/16.
+//  Copyright © 2016 Nimble Chapps. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
